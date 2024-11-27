d74c099f930e2bc7142ebc5dc4310ba42ba030a5
bytelisa <elisa.marzioli2002@gmail.com>
2024-11-27 19:34:27 +0100

    AlreadyFollowingException +

---
+++ src/main/java/org/example/bacheca/exception/AlreadyFollowingException.java
@@ -0,0 +1,10 @@
+package org.example.bacheca.exception;
+
+public class AlreadyFollowingException extends Exception {
+
+    public AlreadyFollowingException() { super();}
+    public AlreadyFollowingException(String message) {
+        super(message);
+    }
+
+}

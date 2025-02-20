36c2354a6830fd065d465abe51affda639ba6633
bytelisa <elisa.marzioli2002@gmail.com>
2025-02-20 13:43:54 +0100

    messaggio

---
+++ src/main/java/org/example/bacheca/controller/MessaggioController.java
@@ -0,0 +1,22 @@
+package org.example.bacheca.controller;
+
+public class MessaggioController implements Controller{
+
+    //controller che si occupa di gestire le interazioni (note): commenti pubblici e messaggi privati
+
+    @Override
+    public void start(){
+
+    }
+
+    public void rispondi(){
+
+    }
+
+    public void elimina(){
+
+    }
+
+
+
+}
